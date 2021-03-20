c8789bd8755c20429c0a4b361fafcdc8656b1231
lichangwen <lichangwen@yy.com>
2021-03-20 15:45:56 +0800

    python hello world

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('firstapp/', hello)
 ]
